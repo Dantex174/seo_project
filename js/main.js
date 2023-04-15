428c60669db0ba23ac5ab6f4ce8d0f07a37c7c50
Dantex <grigory.saifullin@yandex.ru>
2023-04-15 23:35:31 +0300

    Секция FAQ + адаптив

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//Смена цвета линки при переходе на страницу
+const link = document.querySelectorAll('.blockWithLinks__box');
+const linkColor = document.querySelectorAll('.blockWithLinks__title');
+
+function ChangeBgLinK() {
+    
+}
