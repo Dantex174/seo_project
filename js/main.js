1648542abeb31c4d7dec87c1025022814ba0be17
Dantex <grigory.saifullin@yandex.ru>
2023-04-16 01:32:20 +0300

    логика FAQ

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
-'use strict';
+"use strict";
 
-//Смена цвета линки при переходе на страницу
-const link = document.querySelectorAll('.blockWithLinks__box');
-const linkColor = document.querySelectorAll('.blockWithLinks__title');
+const question = document.querySelectorAll('.question'),
+      questionArrow = document.querySelector('.question__icon'),
+      answer = document.querySelectorAll('.question__text'),
+      hidden = document.querySelector('.hiddenAnswer');
 
-function ChangeBgLinK() {
-    
-}
+
+function hideAllAnswers() {
+    answer.forEach(answer => {
+      answer.classList.add('hiddenAnswer');
+    });
+  }
+  
+  function showAnswer() {
+    const question = this.closest('.question');
+    const answer = question.querySelector('.question__text');
+  
+    if (answer.classList.contains('hiddenAnswer')) {
+      hideAllAnswers();
+      answer.classList.remove('hiddenAnswer');
+    } else {
+      answer.classList.add('hiddenAnswer');
+    }
+  }
+  
+  question.forEach((question) => {
+    const questionArrow = question.querySelector('.question__icon');
+    questionArrow.addEventListener('click', showAnswer);
+  });
