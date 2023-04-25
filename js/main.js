9217b2c40bf9f98f6b3f73a1ef50f045bb0e6c57
Dantex <grigory.saifullin@yandex.ru>
2023-04-25 18:09:28 +0300

    убогий код для табов на слотах

--- js/main.js
+++ js/main.js
@@ -28,3 +28,70 @@ function hideAllAnswers() {
     const questionArrow = question.querySelector('.question__icon');
     questionArrow.addEventListener('click', showAnswer);
   });
+
+
+
+  const cardSlots = document.querySelectorAll('.cardSlot'); // Все карточки на странице
+  const imgCards = document.querySelectorAll('.cardSlot__img');
+  const titleCards = document.querySelectorAll('.cardSlot__title');
+  const descrCards = document.querySelectorAll('.cardSlot__descr');
+  const openInfos = document.querySelectorAll('.cardSlot__moreInfo');
+  const closeInfos = document.querySelectorAll('.cardSlot__back');
+  const cardBtn = document.querySelectorAll('.cardSlot__btn');
+  const textAboutSlots = document.querySelectorAll('.cardSlot__textAbout');
+  
+  function openCard(e) {
+      e.preventDefault();
+      const card = e.target.closest('.cardSlot'); // Находим ближайшего родителя с классом .cardSlot
+      const imgCard = card.querySelector('.cardSlot__img');
+      const titleCard = card.querySelector('.cardSlot__title');
+      const descrCard = card.querySelector('.cardSlot__descr');
+      const openInfos = card.querySelector('.cardSlot__moreInfo');
+      const closeInfo = card.querySelector('.cardSlot__back');
+      const cardBtn = card.querySelector('.cardSlot__btn');
+      const textAboutSlot = card.querySelector('.cardSlot__textAbout');
+  
+      imgCard.classList.add('displayNone');
+      titleCard.classList.add('displayNone');
+      descrCard.classList.add('displayNone');
+      cardBtn.classList.add('displayNone');
+      openInfos.classList.add('displayNone');
+      closeInfo.classList.remove('displayNone');
+      textAboutSlot.classList.remove('displayNone');
+  }
+  
+  function closeCard(e) {
+    e.preventDefault();
+      cardSlots.forEach(card => {
+          const imgCard = card.querySelector('.cardSlot__img');
+          const titleCard = card.querySelector('.cardSlot__title');
+          const descrCard = card.querySelector('.cardSlot__descr');
+          const openInfos = card.querySelector('.cardSlot__moreInfo');
+          const closeInfo = card.querySelector('.cardSlot__back');
+          const cardBtn = card.querySelector('.cardSlot__btn');
+          const textAboutSlot = card.querySelector('.cardSlot__textAbout');
+  
+          imgCard.classList.remove('displayNone');
+          titleCard.classList.remove('displayNone');
+          descrCard.classList.remove('displayNone');
+          cardBtn.classList.remove('displayNone');
+          openInfos.classList.remove('displayNone');
+          closeInfo.classList.add('displayNone');
+          textAboutSlot.classList.add('displayNone');
+      });
+  }
+  
+  openInfos.forEach(info => {
+      info.addEventListener('click', (e) => {
+          openCard(e);
+      });
+  });
+  
+  closeInfos.forEach(info => {
+      info.addEventListener('click', (e) => {
+          closeCard(e);
+      });
+  });
+
+
+  
